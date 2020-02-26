f1f5d86c4cdf3e4b0b059fc13818d5eeaed090d8
jasper <msavantfou@gmail.com>
2020-02-26 18:43:22 +1100

    functional chess

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-let express = require("express")
+let express = require("express");
+let socket = require("socket.io");
+let fenArray = [];
+
+let PORT = process.env.PORT||4000;
+let app = express();
+let server = app.listen(PORT, function () {
+    console.log("listening on 4000");
+});
+app.use(express.static("public"));
+var io = socket(server);
+io.on("connection", function (socket) {
+    console.log("connection made"+socket.id);
+    socket.on("fen", function (data) {
+        let fen = data.message;
+        io.sockets.emit("fen",data);
+        console.log(fen);
+        fenArray.push(fen);
+    });
+});
