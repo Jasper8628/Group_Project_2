2bdea137e9be381406faf06dcb2e2484466cc748
Stewart <stewart.ans@gmail.com>
2020-02-26 21:32:05 +1100

    Updates to the file and folder structure

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-let express = require("express")
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./routes/user.js");
+//var routes = require("./routes/gamehistory.js");
+//var routes = require("./routes/chathistory.js");
+
+app.use(routes);
+// app.use(otherroutes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
