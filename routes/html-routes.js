0469c49d759b1abf06ae9eddba82a355add96ae6
Stewart <stewart.ans@gmail.com>
2020-03-04 20:50:58 +1100

    merge back to local

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -22,43 +22,39 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, '../public/register.html'))
   })
 
-  app.get("/profile", function(req, res) {
-
+  app.get('/profile', function (req, res) {
     db.User.findOne({
       where: {
         id: req.user.id
       }
-    }).then(function(data){
-      console.log("-----------------------"+JSON.stringify(data));
-      console.log("-----------------------");
+    }).then(function (data) {
+      console.log('-----------------------' + JSON.stringify(data))
+      console.log('-----------------------')
       var hbsObject = {
         users: data
-      };
-      console.log(hbsObject, hbsObject);
-      res.render("profile", hbsObject);
+      }
+      console.log(hbsObject, hbsObject)
+      res.render('profile', hbsObject)
     })
-
-  });
+  })
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the register page
   app.get('/chess', isAuthenticated, function (req, res) {
     res.sendFile(path.join(__dirname, '../public/chess.html'))
   })
-  
-  app.get("/chat", function(req, res) {
 
-    db.User.findOne({
-      where: {
-        id: req.user.id
-      }
-    }).then(function(data){
-
-      var hbsObject = {
-        users: data
-      };
-      console.log(hbsObject, hbsObject);
-      res.render("chat", hbsObject);
-    })
+  app.get('/chat', function (req, res) {
+    db.User.findOne({
+      where: {
+        id: req.user.id
+      }
+    }).then(function (data) {
+      var hbsObject = {
+        users: data
+      }
+      console.log(hbsObject, hbsObject)
+      res.render('chat', hbsObject)
+    })
   })
 }
