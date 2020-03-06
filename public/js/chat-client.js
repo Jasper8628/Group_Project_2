a7274df700604280fb078a3881feda4111e63aea
jasper <msavantfou@gmail.com>
2020-03-06 21:44:37 +1100

    fixed ports

--- public/js/chat-client.js
+++ public/js/chat-client.js
@@ -3,7 +3,7 @@
  *  Why doesn't any of this need to be in document.ready?
  */
 
-const socket = io('http://localhost:4000')
+const socket = io('https://mighty-caverns-93048.herokuapp.com/')
 //const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
