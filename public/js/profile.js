3636fd386fd49d57b0d8e96d62d2a3b80116d7ee
4gerger <gzhlam@gmail.com>
2020-03-06 22:01:09 +1100

    push

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,16 +2,16 @@ $(document).ready(function () {
   // Getting references from our form and inputs
   var editProfileForm = $('form.profile')
   var emailInput = $('input#emailForm')
-  // var passwordInput = $('input#passwordForm')
-  // var rePasswordInput = $('input#rePasswordForm')
+  var passwordInput = $('input#passwordForm')
+  var rePasswordInput = $('input#rePasswordForm')
 
   // When the edit button is clicked, we update the user table
   editProfileForm.on('submit', function (event) {
       event.preventDefault()
       var userData = {
           email: emailInput.val().trim(),
-          // password: passwordInput.val().trim(),
-          // rePassword: rePasswordInput.val().trim()
+          password: passwordInput.val().trim(),
+          rePassword: rePasswordInput.val().trim()
       }
 
       editProfile(userData.username, userData.email)
