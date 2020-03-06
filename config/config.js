858c8202ae1fed692c727e79e76a8903bea3b329
jasper <msavantfou@gmail.com>
2020-03-06 22:04:23 +1100

    deploying

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     "development": {
-        "username": "root",
-        "password": "Antares28",
-        "database": "chess",
-        "host": "localhost",
+        "username": process.env.MYSQL_USER,
+        "password": process.env.MYSQL_KEY,
+        "database": process.env.MYSQL_DBNAME,
+        "host": process.env.MYSQL_HOST,
         "dialect": "mysql"
     },
     "test": {
@@ -15,10 +15,6 @@ module.exports = {
     },
     "production": {
         "use_env_variable": "JAWSDB_URL",
-        "username": process.env.MYSQL_USER,
-        "password": process.env.MYSQL_KEY,
-        "database": process.env.MYSQL_DBNAME,
-        "host": process.env.MYSQL_HOST,
         "dialect": "mysql"
     }
 }
