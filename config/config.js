ff5d53866684ed0ba6c47ff75ceb70ca6bc4bbba
Seven <sevenspell@gmail.com>
2020-03-07 13:56:12 +1100

    adding heroku prod env code

---
+++ config/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
+}
