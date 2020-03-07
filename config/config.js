73b7574d64bd0a21e3bf35e5d226e543e880043c
jasper <msavantfou@gmail.com>
2020-03-08 08:02:40 +1100

    fixed merge

--- config/config.js
+++ config/config.js
@@ -1,27 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "chess_db",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
-  }
-}
-=======
     "development": {
         "username": "root",
         "password": "Antares28",
@@ -45,4 +22,3 @@ module.exports = {
         "dialect": "mysql"
     }
 }
->>>>>>> 622ec991174dcbaa7e2b8d7906f43ec5f13fff67
